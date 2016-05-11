649b1591d5ad83d1e3e2d28fdf04f985d28aa26e
jone@meilapp.com <jone@meilapp.com>
2016-05-11 16:48:22 +0800

    logentry日志记录查看的实现

--- xadmin/adminx.py
+++ xadmin/adminx.py
@@ -1,3 +1,6 @@
+# coding=utf-8
+from django.contrib.admin.models import LogEntry
+
 import xadmin
 from models import UserSettings
 from xadmin.layout import *
@@ -7,3 +10,10 @@ class UserSettingsAdmin(object):
     model_icon = 'fa fa-cog'
     #hidden_menu = True
 xadmin.site.register(UserSettings, UserSettingsAdmin)
+
+
+class LogEntryAdmin(object):
+    list_display = ['id', '__str__', 'object_id', 'content_type', 'user', 'action_time']
+    list_filter = ['content_type']
+    app_label = 'xadmin'
+xadmin.site.register(LogEntry, LogEntryAdmin)
