f276b73486024e4b02b2e20b62426d3a815d2d80
jone@meilapp.com <jone@meilapp.com>
2016-05-19 12:34:42 +0800

    去掉M2MSelectPlugin加载条件

--- xadmin/plugins/multiselect.py
+++ xadmin/plugins/multiselect.py
@@ -8,18 +8,6 @@ from xadmin import widgets
 
 class M2MSelectPlugin(BaseAdminPlugin):
 
-    def init_request(self, *args, **kwargs):
-        return hasattr(self.admin_view, 'style_fields') and \
-            (
-                'm2m_transfer' in self.admin_view.style_fields.values() or
-                'm2m_dropdown' in self.admin_view.style_fields.values() or
-                
-                'm2m_raw' in self.admin_view.style_fields.values() or
-                'm2m_ajax' in self.admin_view.style_fields.values() or
-                'm2m_ajax_multi' in self.admin_view.style_fields.values() or
-                'm2m_select2' in self.admin_view.style_fields.values()
-            )
-
     def get_field_style(self, attrs, db_field, style, **kwargs):
         if style == 'm2m_transfer' and isinstance(db_field, ManyToManyField):
             return {'widget': widgets.SelectMultipleTransfer(db_field.verbose_name, False), 'help_text': ''}
