32971715d6b4885706ede93a2080de77e3ef951d
jone@meilapp.com <jone@meilapp.com>
2016-05-19 12:37:07 +0800

    名称兼容与同一;去除条件判断

--- xadmin/plugins/relfield.py
+++ xadmin/plugins/relfield.py
@@ -7,14 +7,14 @@ from xadmin import widgets
 class RelateFieldPlugin(BaseAdminPlugin):
 
     def get_field_style(self, attrs, db_field, style, **kwargs):
-        if style == 'fk-ajax' and isinstance(db_field, models.ForeignKey):
-            if (db_field.rel.to in self.admin_view.admin_site._registry) and self.has_model_perm(db_field.rel.to, 'view'):
-                db = kwargs.get('using')
-                return dict(attrs or {}, widget=widgets.ForeignKeySearchWidget(db_field.rel, self.admin_view, using=db))
-        if style == 'fk-raw' and isinstance(db_field, models.ForeignKey):
-            if (db_field.rel.to in self.admin_view.admin_site._registry) and self.has_model_perm(db_field.rel.to, 'view'):
-                db = kwargs.get('using')
-                return dict(attrs or {}, widget=widgets.ForeignKeyRawIdWidget(db_field.rel, self.admin_view, using=db))
+        if (style == 'fk_ajax' or style == 'fk-ajax') and isinstance(db_field, models.ForeignKey):
+            #if (db_field.rel.to in self.admin_view.admin_site._registry) and self.has_model_perm(db_field.rel.to, 'view'):
+            db = kwargs.get('using')
+            return dict(attrs or {}, widget=widgets.ForeignKeySearchWidget(db_field.rel, self.admin_view, using=db))
+        if (style == 'fk_raw' or style == 'fk-raw') and isinstance(db_field, models.ForeignKey):
+            #if (db_field.rel.to in self.admin_view.admin_site._registry) and self.has_model_perm(db_field.rel.to, 'view'):
+            db = kwargs.get('using')
+            return dict(attrs or {}, widget=widgets.ForeignKeyRawIdWidget(db_field.rel, self.admin_view, using=db))
         return attrs
 
 site.register_plugin(RelateFieldPlugin, ModelFormAdminView)
