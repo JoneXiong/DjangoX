69ee9af40924f0873e0287ead8bb8d8baeb39c36
jone@meilapp.com <jone@meilapp.com>
2016-06-02 20:03:25 +0800

    代码结构优化

--- xadmin/views/website.py
+++ xadmin/views/website.py
@@ -2,16 +2,18 @@
 '''
 一些站点级视图
 '''
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy,ugettext as _
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.views.decorators.cache import never_cache
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
 from django.http import HttpResponse
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
 from base import BaseAdminView, filter_hook
 from dashboard import Dashboard
-from xadmin.forms import AdminAuthenticationForm
 from xadmin.models import UserSettings
 from xadmin.layout import FormHelper
 
@@ -36,6 +38,44 @@ class UserSettingView(BaseAdminView):
         us.save()
         return HttpResponse('')
 
+class AdminAuthenticationForm(AuthenticationForm):
+    """
+    A custom authentication form used in the admin app.
+
+    """
+    this_is_the_login_form = forms.BooleanField(
+        widget=forms.HiddenInput, initial=1,
+        error_messages={'required': ugettext_lazy("Please log in again, because your session has expired.")})
+
+    def clean(self):
+        from xadmin.util import User
+        
+        ERROR_MESSAGE = ugettext_lazy("Please enter the correct username and password "
+                                      "for a staff account. Note that both fields are case-sensitive.")
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        message = ERROR_MESSAGE
+
+        if username and password:
+            self.user_cache = authenticate(
+                username=username, password=password)
+            if self.user_cache is None:
+                if u'@' in username:
+                    # Mistakenly entered e-mail address instead of username? Look it up.
+                    try:
+                        user = User.objects.get(email=username)
+                    except (User.DoesNotExist, User.MultipleObjectsReturned):
+                        # Nothing to do here, moving along.
+                        pass
+                    else:
+                        if user.check_password(password):
+                            message = _("Your e-mail address is not your username."
+                                        " Try '%s' instead.") % user.username
+                raise forms.ValidationError(message)
+            elif not self.user_cache.is_active or not self.user_cache.is_staff:
+                raise forms.ValidationError(message)
+        self.check_for_test_cookie()
+        return self.cleaned_data
 
 class LoginView(BaseAdminView):
 
