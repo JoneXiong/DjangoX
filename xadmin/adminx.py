a8a9b9141df83be3f8695acc501e84901aec6dab
jone@meilapp.com <jone@meilapp.com>
2016-06-02 19:50:21 +0800

    add ContentTypeAdmin

--- xadmin/adminx.py
+++ xadmin/adminx.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 from django.contrib.admin.models import LogEntry
+from django.contrib.contenttypes.models import ContentType
 
 import xadmin
 from models import UserSettings
-from xadmin.layout import *
 
 
 class UserSettingsAdmin(object):
     model_icon = 'fa fa-cog'
-    #hidden_menu = True
+    hidden_menu = False
 xadmin.site.register(UserSettings, UserSettingsAdmin)
 
 
@@ -17,3 +17,7 @@ class LogEntryAdmin(object):
     list_filter = ['content_type']
     app_label = 'xadmin'
 xadmin.site.register(LogEntry, LogEntryAdmin)
+
+class ContentTypeAdmin(object):
+    app_label = 'xadmin'
+xadmin.site.register(ContentType, ContentTypeAdmin)
