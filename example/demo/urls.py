4bf969c87cec247999d268a17ca0b0a7e717adef
jone@meilapp.com <jone@meilapp.com>
2016-06-03 15:39:31 +0800

    /  -> /xadmin/; 图片上传相关url

--- example/demo/urls.py
+++ example/demo/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 import xadmin
-xadmin.ROOT_PATH_NAME = ''
+xadmin.ROOT_PATH_NAME = 'xadmin'
+settings.XADMIN_EXCLUDE_PLUGINS = ('bookmark', 'themes')
 xadmin.autodiscover()
 
 # from xadmin.plugins import xversion
@@ -13,5 +15,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(xadmin.site.urls))
+    url(r'^xadmin/', include(xadmin.site.urls)),
+    url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
+    url(r'^uploads/(?P<path>.*)', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}), 
 )
