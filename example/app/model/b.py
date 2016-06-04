66b29d7fb15651548588c2b7744dc87cf3c89023
jone@meilapp.com <jone@meilapp.com>
2016-06-04 18:27:55 +0800

    fix

--- example/app/model/b.py
+++ example/app/model/b.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class B(models.Model):
     name = models.CharField('名称', max_length=500)
-    img = models.ImageField('图片', upload_to='app')
-    img2 = models.ImageField('图片2', upload_to='app',width_field='img2_width',height_field='img2_height',null=True,blank=True)
+    #img = models.ImageField('图片', upload_to='app')
+    #img2 = models.ImageField('图片2', upload_to='app',width_field='img2_width',height_field='img2_height',null=True,blank=True)
     img2_width = models.IntegerField('图片2宽',null=True,blank=True)
     img2_height = models.IntegerField('图片2高',null=True,blank=True)
     
