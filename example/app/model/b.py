cb829c0a88afb1070e5f10cad829362d78d498b6
jone@meilapp.com <jone@meilapp.com>
2016-06-04 18:26:19 +0800

    fix

--- example/app/model/b.py
+++ example/app/model/b.py
@@ -2,11 +2,10 @@
 
 from django.db import models
 
-from xadmin.core.model_fields import CloudImageField
 
 class B(models.Model):
     name = models.CharField('名称', max_length=500)
-    img = CloudImageField('图片', upload_to='app')
+    img = models.ImageField('图片', upload_to='app')
     img2 = models.ImageField('图片2', upload_to='app',width_field='img2_width',height_field='img2_height',null=True,blank=True)
     img2_width = models.IntegerField('图片2宽',null=True,blank=True)
     img2_height = models.IntegerField('图片2高',null=True,blank=True)
