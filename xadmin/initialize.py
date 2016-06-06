f383dc472ec330b86272f9fd6be572b57899b1db
jone@meilapp.com <jone@meilapp.com>
2016-06-06 23:56:51 +0800

    兼容django1.9使用标准库中的import_module

--- xadmin/initialize.py
+++ xadmin/initialize.py
@@ -1,7 +1,10 @@
 # coding=utf-8
 
 from django.conf import settings
-from django.utils.importlib import import_module
+try:
+    from django.utils.importlib import import_module
+except:
+    from importlib import import_module
 from django.utils.module_loading import module_has_submodule
 
 
