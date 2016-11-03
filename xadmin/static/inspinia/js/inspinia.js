516873ea7dbfd1f8f717a53a8ef7cb7e7b720523
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2016-11-03 10:26:40 +0800

    inspinia lib fix

--- xadmin/static/inspinia/js/inspinia.js
+++ xadmin/static/inspinia/js/inspinia.js
@@ -98,10 +98,10 @@ $(document).ready(function () {
 
     // Append config box / Only for demo purpose
     // Uncomment on server mode to enable XHR calls
-   $.get("skin-config.html", function (data) {
-       if (!$('body').hasClass('no-skin-config'))
-           $('body').append(data);
-   });
+//   $.get("/static/inspinia/skin-config.html", function (data) {
+//       if (!$('body').hasClass('no-skin-config'))
+//           $('body').append(data);
+//   });
 
     // Minimalize menu
     $('.navbar-minimalize').click(function () {
