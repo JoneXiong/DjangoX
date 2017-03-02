aab02416c06f497e018f0d1e074a5b747d32823f
jone <xiongjianhong@gmail.com>
2017-03-02 17:28:39 +0800

    example: fix

--- example/start/urls.py
+++ example/start/urls.py
@@ -1,6 +1,7 @@
-import django 
+import django
 from django.conf.urls import include, url#,patterns
 from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 import xadmin
@@ -9,15 +10,10 @@ settings.XADMIN_EXCLUDE_PLUGINS = ['bookmark']
 
 xadmin.autodiscover()
 
-# from xadmin.plugins import xversion
-# xversion.register_models()
 
 from django.contrib import admin
 admin.autodiscover()
 
-
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
