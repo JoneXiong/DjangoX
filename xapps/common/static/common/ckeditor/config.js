31559605861ace9f2028e48e7e2c817ecb74260a
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2017-08-21 11:25:16 +0800

    增加一些ckeditor插件实现拖拽上传图片

--- xapps/common/static/common/ckeditor/config.js
+++ xapps/common/static/common/ckeditor/config.js
@@ -3,9 +3,20 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-    config.filebrowserUploadUrl="/xadmin/ckupload/"
+CKEDITOR.editorConfig = function(config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.extraPlugins = 'notification';
+    config.extraPlugins = 'notificationaggregator';
+    config.extraPlugins = 'filetools';
+    config.extraPlugins = 'clipboard';
+    config.extraPlugins = 'widget';
+    config.extraPlugins = 'lineutils';
+    config.extraPlugins = 'widgetselection';
+    config.extraPlugins = 'selectall';
+    config.extraPlugins = 'uploadwidget';
+    config.extraPlugins = 'uploadimage';
+    config.filebrowserUploadUrl = "/xadmin/ckupload/";
+    config.imageUploadUrl = "/xadmin/ckupdrogload/";
 };
--- xapps/common/views/upload_view_bak.py
+++ xapps/common/views/upload_view_bak.py
@@ -4,6 +4,7 @@ import time
 from django import forms
 from django.views.generic import View
 from django.http import HttpResponse, JsonResponse
+from django.conf import settings
 
 from xadmin import site
 from xadmin.views.base import BaseView
@@ -17,14 +18,20 @@ class UploadView(BaseView):
         try:
             path = "uploads/" + time.strftime("%Y%m%d%H%M%S",time.localtime())
             f = request.FILES["upload"]
-            file_name = path + "_" + f.name
-            des_origin_f = open(file_name, "wb+")
-            for chunk in f.chunks():
-                des_origin_f.write(chunk)
-            des_origin_f.close()
+
+            from xadmin.core.storage_qiniu import save_imgs
+            imgs = save_imgs([f])
+            url = settings.REMOTE_MEDIA_URL + imgs[0]['img']
+
+            #file_name = path + "_" + f.name
+            #des_origin_f = open(file_name, "wb+")
+            #for chunk in f.chunks():
+            #    des_origin_f.write(chunk)
+            #des_origin_f.close()
+            #url = '/' + file_name
         except Exception, e:
             import traceback;traceback.print_exc()
-        res = "<script>window.parent.CKEDITOR.tools.callFunction("+callback+",'/"+file_name+"', '');</script>"
+        res = "<script>window.parent.CKEDITOR.tools.callFunction("+callback+",'"+url+"', '');</script>"
         return HttpResponse(res)
 site.register_view(r'^ckupload/$', UploadView, name='ckupload')
 
@@ -37,15 +44,21 @@ class UploadDrogImgView(BaseView):
             path = "uploads/" + time.strftime("%Y%m%d%H%M%S", time.localtime())
             file_dir_name = path.split('/')[-1]
             f = request.FILES["upload"]
-            file_name = path + "_" + f.name
-            with open(file_name, "wb+") as des_origin_f:
-                for chunk in f.chunks():
-                    des_origin_f.write(chunk)
-            des_origin_f.close()
+
+            from xadmin.core.storage_qiniu import save_imgs
+            imgs = save_imgs([f])
+            url = settings.REMOTE_MEDIA_URL + imgs[0]['img']
+
+            #file_name = path + "_" + f.name
+            #with open(file_name, "wb+") as des_origin_f:
+            #    for chunk in f.chunks():
+            #        des_origin_f.write(chunk)
+            #des_origin_f.close()
+            #url = '/' + file_name
             success_message = {
                 "uploaded": 1,
                 "fileName": file_dir_name + "_" + f.name,
-                "url": '/' + file_name
+                "url": url
             }
             return JsonResponse(success_message)
         except:
