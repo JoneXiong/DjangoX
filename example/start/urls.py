3991203b14be5d41f094bd38a61da9db8fa21aba
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2017-10-12 12:14:41 +0000

    example: 首页自动跳转

--- example/start/urls.py
+++ example/start/urls.py
@@ -2,6 +2,7 @@ import django
 from django.conf.urls import include, url#,patterns
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 import xadmin
@@ -18,4 +19,5 @@ xadmin.autodiscover()
 urlpatterns = [
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^xadmin/', include(xadmin.site.urls)),
+    url(r'^$',RedirectView.as_view(url='/xadmin/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
